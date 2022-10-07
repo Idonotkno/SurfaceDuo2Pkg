c6c5d5e509beb57cc66bb4ce977646d8674ce537
ww <412353716@qq.com>
2022-10-08 01:40:29 +0800

    Delete build_uefi_epsilon.sh

--- build_uefi_epsilon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-stuart_setup -c Platforms/SurfaceDuo1Pkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-stuart_update -c Platforms/SurfaceDuo1Pkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-stuart_build -c Platforms/SurfaceDuo1Pkg/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
